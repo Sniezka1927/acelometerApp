54fd453d8bcb071c3d4db332670b38fbfe82c550
Sniezka1927 <dygons14@gmail.com>
2022-12-14 16:25:38 +0100

    a

--- client/script.js
+++ client/script.js
@@ -9,9 +9,7 @@ const paramaters = {
 window.addEventListener("load", () => {
   const ws = new WebSocket(`ws://${serverAddress}`);
 
-  ws.onopen = () => {
-    //   ws.send("klient się podłączył");
-  };
+  ws.onopen = () => {};
 
   ws.onmessage = (e) => {
     const data = JSON.parse(e.data);
@@ -30,41 +28,24 @@ window.addEventListener("load", () => {
     console.log(e.code, e.reason);
   };
 });
-// Gen random data
-const N = 300;
-const gData = [...Array(N).keys()].map(() => ({
-  lat: (Math.random() - 0.5) * 180,
-  lng: (Math.random() - 0.5) * 360,
-  alt: Math.random(),
-  radius: Math.random() * 5,
-  color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-}));
-
 const Globe = new ThreeGlobe()
   .globeImageUrl("//unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
-  .bumpImageUrl("//unpkg.com/three-globe/example/img/earth-topology.png")
-  .customLayerData(gData)
-  .customThreeObject(
-    (d) =>
-      new THREE.Mesh(
-        new THREE.SphereGeometry(d.radius),
-        new THREE.MeshLambertMaterial({ color: d.color })
-      )
-  )
-  .customThreeObjectUpdate((obj, d) => {
-    Object.assign(obj.position, Globe.getCoords(d.lat, d.lng, d.alt));
-  });
+  .bumpImageUrl("//unpkg.com/three-globe/example/img/earth-topology.png");
 
-console.log(Globe.position);
-// Globe.position.x = 50;
-// Globe.position.y = 50;
-Globe.position.z = 100;
+// custom globe material
+const globeMaterial = Globe.globeMaterial();
+globeMaterial.bumpScale = 10;
+new THREE.TextureLoader().load(
+  "//unpkg.com/three-globe/example/img/earth-water.png",
+  (texture) => {
+    globeMaterial.specularMap = texture;
+    globeMaterial.specular = new THREE.Color("grey");
+    globeMaterial.shininess = 15;
+  }
+);
 
-(function moveSpheres() {
-  gData.forEach((d) => (d.lat += 0.2));
-  Globe.customLayerData(Globe.customLayerData());
-  requestAnimationFrame(moveSpheres);
-})();
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.position.set(1, 1, 1); // change light position to see the specularMap's effect
 
 // Setup renderer
 const renderer = new THREE.WebGLRenderer();
@@ -75,21 +56,18 @@ document.getElementById("globeViz").appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 scene.add(Globe);
 scene.add(new THREE.AmbientLight(0xbbbbbb));
-scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
+scene.add(directionalLight);
 
 // Setup camera
 const camera = new THREE.PerspectiveCamera();
 camera.aspect = window.innerWidth / window.innerHeight;
 camera.updateProjectionMatrix();
-camera.position.set(00, 0, 500);
-
 camera.position.z = 500;
 
 // Kick-off renderer
 (function animate() {
   // IIFE
   // Frame cycle
-  // tbControls.update();
   Globe.rotation.x = paramaters.x * 6;
   Globe.rotation.y = paramaters.y * 6;
   renderer.render(scene, camera);
